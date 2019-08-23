892b0e7968c3b62020fdb9944b70e59e71dc659f
irpanm <rachmatirfanm@gmail.com>
2019-08-24 06:23:09 +0700

    fix status matkul

--- models/Rekapan_perhari_model.php
+++ models/Rekapan_perhari_model.php
@@ -3,7 +3,13 @@
 Class Rekapan_perhari_model extends CI_Model{
 
     public function tampilGroupByHari($haritanggal,$semester,$tahun_ajaran){
-        return $this->db->query("select * from jadwal where haritanggal='$haritanggal' and semester='$semester' and tahun_ajaran='$tahun_ajaran' order by haritanggal asc")->result_array();
+        return $this->db->query("select * from jadwal a
+        inner join matkul b ON a.makul=b.makul
+        where a.haritanggal='$haritanggal' 
+        and a.semester='$semester' and a.tahun_ajaran='$tahun_ajaran' 
+        and b.semester='$semester' and b.tahun_ajaran='$tahun_ajaran' 
+        and b.status = 1
+        order by a.id asc")->result_array();
     }
     public function getPerhari($semester,$tahun_ajaran){
         return $this->db->query("select haritanggal from jadwal where semester='$semester' and tahun_ajaran='$tahun_ajaran' group by haritanggal order by id asc")->result_array();
